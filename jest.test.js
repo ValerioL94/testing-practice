c181c57c14836daf6b7516719bb090de1395e273
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-02-22 13:49:25 +0100

    Add working reverseString(string)

--- jest.test.js
+++ jest.test.js
@@ -1,5 +1,9 @@
 import capitalize from './modules/capitalize';
-
+import reverseString from './modules/reverseString';
 test('capitalize first character', () => {
   expect(capitalize('interconnection')).toMatch(/[A-Z]\w+/);
+});
+
+test('reverse a string', () => {
+  expect(reverseString('interconnection')).toBe('noitcennocretni');
 });
